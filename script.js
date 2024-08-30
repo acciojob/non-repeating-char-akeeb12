534655402d75e1c0eb77def818ce669299c0187f
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-08-30 08:38:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let frequencyMap = {};
+
+	for(let i = 0; i < str.length; i ++)
+		{
+			let char = str[i];
+			if (frequencyMap[char]) {
+				frequencyMap[char]++;
+			}else
+				frequencyMap[char] = 1
+		}
 }
+
+for(let i = 0; i < str.length; i++)
+	{
+		let char = str[i];
+		if (frequencyMap[char] === 1) {
+			return char;
+		}
+	}
+
+retun null;
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
+
+
+
+
+
+
